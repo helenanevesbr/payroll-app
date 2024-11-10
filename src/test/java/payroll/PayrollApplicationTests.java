b69967aa77288ee00a054f96a26fb790c77c1b36
Helena Neves <helenaneves.br@gmail.com>
2024-11-10 19:25:54 -0300

    doc: rename project

--- src/test/java/payroll/PayrollApplicationTests.java
+++ src/test/java/payroll/PayrollApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelenaApplicationTests {
+class PayrollApplicationTests {
 
 	@Test
 	void contextLoads() {
